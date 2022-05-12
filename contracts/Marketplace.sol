40e863434e7af653ad93a2f6c088181cfaaf1f44
LUDO\Ludovic <ludovic.domingues96@gmail.com>
2022-05-12 19:07:59 +0200

    Moved marketplace and using it as main contract

---
+++ contracts/Marketplace.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import "./SalesHandler.sol";
+import "./CollectionHandler.sol";
+
+contract Marketplace is CollectionHandler, SalesHandler{}
