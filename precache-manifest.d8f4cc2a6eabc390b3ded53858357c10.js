1f4502b9f3912070ce42a51a81eb130c522719cc
Eorg79 <erika@LAPTOP-E4RLR8OC.localdomain>
2022-05-15 23:43:14 +0200

    Updates

---
+++ precache-manifest.d8f4cc2a6eabc390b3ded53858357c10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87a2c9a91664e9d9b2109bb02b36106d",
+    "url": "/NFTPlatform/Client/index.html"
+  },
+  {
+    "revision": "4874214ac855b504479f",
+    "url": "/NFTPlatform/Client/static/css/main.518ae982.chunk.css"
+  },
+  {
+    "revision": "cd43aeb3286732675f0b",
+    "url": "/NFTPlatform/Client/static/js/2.54f89225.chunk.js"
+  },
+  {
+    "revision": "4874214ac855b504479f",
+    "url": "/NFTPlatform/Client/static/js/main.d5dabbd3.chunk.js"
+  },
+  {
+    "revision": "77a1f8abda3fd6d1cc02",
+    "url": "/NFTPlatform/Client/static/js/runtime-main.2e299f6d.js"
+  },
+  {
+    "revision": "87c06791967f8ab23548416189397507",
+    "url": "/NFTPlatform/Client/static/media/background.87c06791.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/NFTPlatform/Buy/precache-manifest.3903b16201fbae029373c841bde246b2.js"
+  "/NFTPlatform/Client/precache-manifest.d8f4cc2a6eabc390b3ded53858357c10.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/NFTPlatform/Buy/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/NFTPlatform/Client/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
