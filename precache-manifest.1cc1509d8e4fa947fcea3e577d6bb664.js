60a8ad8098077266007bcf386139ce045688e85d
Eorg79 <erika@LAPTOP-E4RLR8OC.localdomain>
2022-05-15 21:39:25 +0200

    Updates

---
+++ precache-manifest.1cc1509d8e4fa947fcea3e577d6bb664.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc953f9bd16127f890492a99bc8eda6b",
+    "url": "/NFTPlatform/index.html"
+  },
+  {
+    "revision": "3559ce83f423adc98c69",
+    "url": "/NFTPlatform/static/css/main.d341498a.chunk.css"
+  },
+  {
+    "revision": "697bd8cb5a7f5a6e01da",
+    "url": "/NFTPlatform/static/js/2.e0fe09f2.chunk.js"
+  },
+  {
+    "revision": "3559ce83f423adc98c69",
+    "url": "/NFTPlatform/static/js/main.a95cd2c4.chunk.js"
+  },
+  {
+    "revision": "44ab174219779c5c5e33",
+    "url": "/NFTPlatform/static/js/runtime-main.7a8634c7.js"
+  },
+  {
+    "revision": "87c06791967f8ab23548416189397507",
+    "url": "/NFTPlatform/static/media/background.87c06791.png"
+  }
+]);
